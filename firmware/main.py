b3c4ab74b4ea9ff095f7ca2c093c8337f56f223f
Witold Potrykus <fastboy158@gmail.com>
2026-07-07 15:23:58 +0200

    added trainer and hardware modules

--- firmware/main.py
+++ firmware/main.py
@@ -1,38 +1,9 @@
-import time
-import config
-from input import Input
-from audio import Audio
-from display import Display
 from keyer import Keyer
-import morse
 
-inp = Input()
-audio = Audio()
-disp = Display()
-keyer = Keyer(audio)
+k = Keyer(18)
 
-disp.show("CW TRAINER", "READY")
+print()
 
-while True:
+print("CW Trainer")
 
-    state = inp.read()
-
-    if state["dit"]:
-        keyer.dit()
-        disp.show("DIT", keyer.buffer)
-        while inp.read()["dit"]:
-            pass
-
-    elif state["dah"]:
-        keyer.dah()
-        disp.show("DAH", keyer.buffer)
-        while inp.read()["dah"]:
-            pass
-
-    if keyer.ready_to_decode() and keyer.buffer:
-        letter = morse.decode(keyer.buffer)
-        disp.show(keyer.buffer, letter)
-        print(keyer.buffer, "=", letter)
-        keyer.buffer = ""
-
-    time.sleep_ms(10)
+print(k.get_timings())
